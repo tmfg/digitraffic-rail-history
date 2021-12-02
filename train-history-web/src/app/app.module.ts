837352f74d8d6d329b062f736ffaf355c3b03c0b
Jaakko Paavilainen <jaakko.paavilainen@solita.fi>
2021-12-02 12:46:09 +0200

    DPO-1599: Angular 4->5

--- train-history-web/src/app/app.module.ts
+++ train-history-web/src/app/app.module.ts
@@ -1,20 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { TrainTableComponent } from './components/train-table/train-table.component';
+import {AppComponent} from './app.component';
+import {TrainTableComponent} from './components/train-table/train-table.component';
 import {HashLocationStrategy, LocationStrategy} from "@angular/common";
 import {TrainService} from "./services/train.service";
-import {HttpModule} from "@angular/http";
 import {FormsModule} from "@angular/forms";
-import { CompositionTableComponent } from './components/composition-table/composition-table.component';
+import {CompositionTableComponent} from './components/composition-table/composition-table.component';
 import {CompositionService} from "./services/composition.service";
 import {RouterModule, Routes} from "@angular/router";
+import {HttpClientModule} from "@angular/common/http";
 
 
 const appRoutes: Routes = [
-  { path: 'compositions', component: CompositionTableComponent },
-  { path: 'trains',      component: TrainTableComponent }
+  {path: 'compositions', component: CompositionTableComponent},
+  {path: 'trains', component: TrainTableComponent}
 ];
 
 
@@ -25,10 +25,10 @@ const appRoutes: Routes = [
     CompositionTableComponent
   ],
   imports: [
-    BrowserModule, HttpModule, FormsModule,
+    BrowserModule, HttpClientModule, FormsModule,
     RouterModule.forRoot(
       appRoutes,
-      { enableTracing: false } // <-- debugging purposes only
+      {enableTracing: false} // <-- debugging purposes only
     )
   ],
   bootstrap: [AppComponent],
@@ -37,4 +37,5 @@ const appRoutes: Routes = [
     useClass: HashLocationStrategy
   }, TrainService, CompositionService]
 })
-export class AppModule { }
+export class AppModule {
+}
