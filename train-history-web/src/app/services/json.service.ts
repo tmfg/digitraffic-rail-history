1998092be4bde48f591221baf0858bf4f1f0e128
Jaakko Paavilainen <jaakko.paavilainen@solita.fi>
2021-12-02 12:40:13 +0200

    Angular 4->5

--- train-history-web/src/app/services/json.service.ts
+++ train-history-web/src/app/services/json.service.ts
@@ -1,29 +1,17 @@
-import {Http} from '@angular/http';
 import 'rxjs/Rx';
 import {Injectable} from "@angular/core";
 import {Observable} from "rxjs/Observable";
+import {HttpClient} from '@angular/common/http';
 
 @Injectable()
 export class JsonService {
 
-  constructor(private http: Http) {
-  }
-
-  private jsonToEntity(json: any): any {
-    var output = [];
-
-    var jsonDoc = json.json();
-    for (var i = 0; i < jsonDoc.length; i++) {
-      var item = jsonDoc[i];
-      Object.assign(item, jsonDoc[i].json);
-      output.push(item);
-    }
-    return output;
+  constructor(private http: HttpClient) {
   }
 
   public getJSON(trainNumber: number, departureDate: string, entityName: string): Observable<any> {
-    let url = `/api/v1/${ entityName }s/history/${departureDate}/${trainNumber}`
-    return this.http.get(url).map(this.jsonToEntity).publishLast().refCount();
+    let url = `/api/v1/${entityName}s/history/${departureDate}/${trainNumber}`
+    return this.http.get(url).publishLast().refCount();
   }
 
 }
--- train-history-web/src/app/services/train.service.ts
+++ train-history-web/src/app/services/train.service.ts
@@ -1,15 +1,13 @@
-import {Http, Response, Headers, RequestOptions} from '@angular/http';
 import 'rxjs/Rx';
 import {Injectable} from "@angular/core";
 import {Observable} from "rxjs/Observable";
-import { environment } from '../../environments/environment';
 import {JsonService} from "./json.service";
 
 @Injectable()
-export class TrainService extends JsonService{
+export class TrainService extends JsonService {
 
   public getJSON(trainNumber: number, departureDate: string): Observable<any> {
-    return super.getJSON(trainNumber,departureDate,"train");
+    return super.getJSON(trainNumber, departureDate, "train");
   }
 
 }
