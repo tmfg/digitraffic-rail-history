f2011881fc5f93f880395bcaa1a3f0d15c7a40d3
Matti Heinonen <matti.heinonen@solita.fi>
2022-08-30 12:21:22 +0300

    DPO-1925 Remove SpringBootTest-annotation + enable tests during build. Database for tests has never been set up, and it won't work.

--- train-history-updater/src/test/java/fi/livi/trainhistoryupdater/TrainHistoryApplicationTests.java
+++ train-history-updater/src/test/java/fi/livi/trainhistoryupdater/TrainHistoryApplicationTests.java
@@ -1,11 +1,8 @@
 package fi.livi.trainhistoryupdater;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 public class TrainHistoryApplicationTests {
-
 	@Test
 	public void contextLoads() {
 	}
