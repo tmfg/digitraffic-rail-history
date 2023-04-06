2565e0c5e3ac2ca1b0ef0f6995167d6dabd0d63c
Jani Haakana <jani.k.haakana@solita.fi>
2023-04-06 15:50:28 +0300

    Update materialui 14 --> 15

--- train-history-web/src/app/app.module.ts
+++ train-history-web/src/app/app.module.ts
@@ -11,12 +11,12 @@ import {CompositionService} from "./services/composition.service";
 import {RouterModule, Routes} from "@angular/router";
 import {HttpClientModule} from "@angular/common/http";
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatSelectModule} from "@angular/material/select";
-import {MatInputModule} from "@angular/material/input";
+import {MatLegacySelectModule as MatSelectModule} from "@angular/material/legacy-select";
+import {MatLegacyInputModule as MatInputModule} from "@angular/material/legacy-input";
 import {MatIconModule} from "@angular/material/icon";
-import {MatButtonModule} from "@angular/material/button";
-import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
-import {MatTableModule} from "@angular/material/table";
+import {MatLegacyButtonModule as MatButtonModule} from "@angular/material/legacy-button";
+import {MatLegacyProgressSpinnerModule as MatProgressSpinnerModule} from "@angular/material/legacy-progress-spinner";
+import {MatLegacyTableModule as MatTableModule} from "@angular/material/legacy-table";
 
 
 const appRoutes: Routes = [
