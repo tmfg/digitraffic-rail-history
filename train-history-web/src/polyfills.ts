34d376ba09d549e5c33226ee78aeddfd0dff4aac
Jani Haakana <jani.k.haakana@solita.fi>
2023-04-28 14:43:04 +0300

    Fix styling errors and update corejs

--- train-history-web/src/polyfills.ts
+++ train-history-web/src/polyfills.ts
@@ -34,19 +34,13 @@
 // import 'core-js/es6/weak-map';
 // import 'core-js/es6/set';
 
-
 /** Evergreen browsers require these. **/
-import 'core-js/es6/reflect';
-import 'core-js/es7/reflect';
-
-
+import "core-js/es/reflect";
 
 /***************************************************************************************************
  * Zone JS is required by Angular itself.
  */
-import 'zone.js';  // Included with Angular CLI.
-
-
+import "zone.js"; // Included with Angular CLI.
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
