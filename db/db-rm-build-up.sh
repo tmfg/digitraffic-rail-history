9b607d3457f0a853937d4e98e3058b379f2a5e47
jouniso <jouniso@users.noreply.github.com>
2025-02-28 10:32:58 +0100

    DPO-1018 RATA: Katselmoidaan ympäristö ja komponenttien tehokkuus ja koko - Fixed documentation and scripts

--- db/db-rm-build-up.sh
+++ db/db-rm-build-up.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ -x "$(command -v docker-compose)" ]; then
-  docker-compose down && docker-compose rm db && docker-compose build && docker-compose up
+  docker-compose down && docker-compose rm rail_history_mysql && docker-compose build && docker-compose up
 else
-  docker compose down && docker compose rm db && docker compose build && docker compose up
+  docker compose down && docker compose rm rail_history_mysql && docker compose build && docker compose up
 fi
